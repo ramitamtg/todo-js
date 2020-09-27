ee005d1f1e2cb4a58be55c21424af6a7bec8cdfd
ramitaDev <ramirodoninodt@gmail.com>
2020-09-27 01:01:33 -0300

    Primer commit

---
+++ src/classes/index.js
@@ -0,0 +1,9 @@
+//Este archivo sirve para importar todos mis archivos y luego importarlos juntos 
+
+import {Todo} from "./todo.class"
+import {TodoList} from "./todo-list.class"
+
+export {
+    Todo,
+    TodoList
+}
